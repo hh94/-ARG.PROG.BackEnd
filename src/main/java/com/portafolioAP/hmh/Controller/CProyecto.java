5910b4f52f7284ec8d4297acd4596f65c6c75774
hh94 <hirohiga.hh@gmail.com>
2023-03-24 15:39:36 -0300

    46

--- src/main/java/com/portafolioAP/hmh/Controller/CProyecto.java
+++ src/main/java/com/portafolioAP/hmh/Controller/CProyecto.java
@@ -23,7 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pro")
-@CrossOrigin(origins = "https://fe-argprog.web.app")
+@CrossOrigin(origins = "https://fe-argprog.firebaseapp.com")
+//@CrossOrigin(origins = "https://fe-argprog.web.app")
 //@CrossOrigin(origins = "http://localhost:4200")
 public class CProyecto {
     @Autowired
