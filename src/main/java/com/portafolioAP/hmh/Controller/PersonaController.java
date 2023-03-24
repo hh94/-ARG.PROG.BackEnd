65d767da6bb9798cb51021ad18dc941508f6f4e1
hh94 <hirohiga.hh@gmail.com>
2023-03-24 15:26:03 -0300

    45

--- src/main/java/com/portafolioAP/hmh/Controller/PersonaController.java
+++ src/main/java/com/portafolioAP/hmh/Controller/PersonaController.java
@@ -17,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
   
   
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://fe-argprog.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
